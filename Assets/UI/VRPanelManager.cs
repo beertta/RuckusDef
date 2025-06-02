e0bd52909e925fa7f99a5a08e54b309d6284222a
beertta <berta.esteve.escoda@estudiantat.upc.edu>
2025-06-02 19:48:07 +0200

    UI con cuenta atrás
    Co-Authored-By: Pusa <armand.puertos@estudiantat.upc.edu>
    Co-Authored-By: clara_ <181961220+ClaraUpc@users.noreply.github.com>
    Co-Authored-By: Daniel Gubianas <134320635+Dani3333333@users.noreply.github.com>
    Co-Authored-By: OscarVisual <182936687+OscarVisual@users.noreply.github.com>
    Co-Authored-By: SWIPAX-fantasy <184212651+SWIPAX-fantasy@users.noreply.github.com>
    Co-Authored-By: piplesco <184211963+piplesco@users.noreply.github.com>
    Co-Authored-By: raquelcusa <184212157+raquelcusa@users.noreply.github.com>

--- Assets/UI/VRPanelManager.cs
+++ Assets/UI/VRPanelManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class VRPanelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+
+    public TextMeshProUGUI countdownText1;
+    public TextMeshProUGUI countdownText2;
+    public TextMeshProUGUI countdownText3;
+
     void Start()
     {
-        
+        StartCoroutine(PanelSequence());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PanelSequence()
     {
-        
+        // Panel 1: 5 segundos
+        panel1.SetActive(true);
+        panel2.SetActive(false);
+        panel3.SetActive(false);
+        yield return StartCoroutine(Countdown(5, countdownText1));
+        panel1.SetActive(false);
+
+        // Panel 2: 2 segundos
+        panel2.SetActive(true);
+        yield return StartCoroutine(Countdown(2, countdownText2));
+        panel2.SetActive(false);
+
+        // Panel 3: 5 segundos
+        panel3.SetActive(true);
+        yield return StartCoroutine(Countdown(10, countdownText3));
+        panel3.SetActive(false);
+
+        // Iniciar el juego
+        StartGame();
+    }
+
+    IEnumerator Countdown(int seconds, TextMeshProUGUI textElement)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            textElement.text = i.ToString();
+            yield return new WaitForSeconds(1f);
+        }
+        textElement.text = ""; // Limpia al final si quieres
+    }
+
+    void StartGame()
+    {
+        Debug.Log(" ¡Empieza el juego!");
+        // Aquí tu lógica para iniciar el juego en VR
     }
 }
