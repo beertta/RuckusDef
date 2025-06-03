1b1bf7d570411a7ce713b37360eb456929402f62
Daniel Gubianas <daniel.gubianas@estudiantat.upc.edu>
2025-06-03 03:17:30 +0200

    hemos cambiado algunos paneles que estavan a overlay a worldspace
    todos

--- Assets/Code/EnemySpawner.cs
+++ Assets/Code/EnemySpawner.cs
@@ -66,7 +66,7 @@ public class EnemySpawner : MonoBehaviour
 
         yield return StartCoroutine(ActivarPanelYEsperar(round2Panel));
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 7; i++)
         {
             yield return EsperarHastaQueNoHayaEnemigo();
             yield return new WaitForSeconds(spawnInterval);
